cda0ec07ec4a4719d5d118fc589c5c24c3d877d3
Valais Girl <Yevette@Hal-71.local>
2023-05-13 17:53:32 -0500

    writeFile shapes function

---
+++ index.js
@@ -0,0 +1,28 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const { Triangle, Square, Circle } = require(".lib/shapes");
+
+function writeToFile(fileName, answers){
+    let svgString = "";
+    svgString =
+    `svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">`;
+    svgString += `${answers.shape}`;
+
+    let shapeChoice;
+    if (answers.shape ==="Triangle") {
+        shapeChoice = new Triangle();
+        svgString+= `<polygon points="145,10 285,180 10,180" fill="${answers.shapeColor}"/>`
+    }else if (answers.shape ==="Square") {
+        shapeChoice = new Square();
+        svgString+= `<rect x="50" y="25" width="200" height="200" fill="${answers.shapeColor}"/>`
+    }else if (answers.shape ==="Circle") {
+        shapeChoice = new Circle();
+        svgString+= `<circle cx="150" cy="100" r="80" fill="${answers.shapeColor}"/>`
+    }
+    svgString += `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${answers.textColor}">${answers.text}</text>`;
+    svgString += "</svg>";
+};
+
+fs.writeFile(fileName, svgString, (err) => {
+    err ? console.log(err) : console.log("Generated logo.svg")
+});
